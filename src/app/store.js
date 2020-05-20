a9fbc4d875542b7d050490ca60be0ec08faa2caf
alex <jesus.galaz@softtek.com>
2020-05-20 06:51:59 -0500

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+import monitorReducersEnhancer from './enhancers/monitorReducers'
+import loggerMiddleware from './middleware/logger'
+import rootReducer from './reducers'
+
+export default function configureAppStore(preloadedState) {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: [loggerMiddleware, ...getDefaultMiddleware()],
+    preloadedState,
+    enhancers: [monitorReducersEnhancer],
+  })
+
+  if (process.env.NODE_ENV !== 'production' && module.hot) {
+    module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+  }
+
+  return store
+}
